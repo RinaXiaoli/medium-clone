d6f1613e297667643ee01614bf67cd1ae872e882
Романова Ирина Александровна <ia.romanova@adpo.intranet>
2021-01-27 08:47:49 +0300

    #1 module9 useRef

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from 'routes';
+import {BrowserRouter as Router} from 'react-router-dom';
+import TopBar from 'components/topBar';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
